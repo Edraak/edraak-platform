44224d718edd0679f669cd3e3c0f5e41a952ef70
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-02-14 14:19:19 +0200

    Enable accessibility tests

--- scripts/accessibility-tests.sh
+++ scripts/accessibility-tests.sh
@@ -16,7 +16,7 @@ echo "Setting up for accessibility tests..."
 source scripts/jenkins-common.sh
 
 echo "Running explicit accessibility tests..."
-SELENIUM_BROWSER=phantomjs paver test_a11y --with-xunitmp
+SELENIUM_BROWSER=phantomjs paver test_a11y --with-xunit
 
 echo "Generating coverage report..."
 paver a11y_coverage
