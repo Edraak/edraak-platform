d47ed2ab9b8cdbf392efd2df2a6344e6cb262674
Omar Al-Ithawi <i@omardo.com>
2017-02-28 10:33:23 +0200

    Migrating edraak_ratelimit module with test cases and quality improvments

---
+++ common/djangoapps/edraak_ratelimit/backends.py
@@ -0,0 +1,73 @@
+"""
+Edraak authentication backends apply rate limits.
+"""
+
+from ratelimitbackend.backends import RateLimitMixin
+from ratelimitbackend.exceptions import RateLimitException
+from django.db.models import F
+
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
+
+from edraak_ratelimit.models import RateLimitedIP
+
+
+class EdraakRateLimitMixin(RateLimitMixin):
+    """
+    Make the limit a little bit more permissive.
+    """
+
+    # Edraak (ratelimit): We're overriding the values to avoid patching ratelimit pip package itself.
+    minutes = 5
+    requests = 100  # Make the limit a little bit more permissive
+
+    def db_log_failed_attempt(self, request, username=None):
+        """
+        Store information about the failed attempt in the database.
+
+        Args:
+            request: WSGIRequest object.
+        """
+        user = None
+        ip_address = request.META['REMOTE_ADDR']
+
+        if username:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                pass
+
+        if not user:
+            if hasattr(request, 'user') and request.user.is_authenticated():
+                user = request.user
+
+        try:
+            limited_ip = RateLimitedIP.objects.get(ip_address=ip_address)
+            limited_ip.lockout_count = F('lockout_count') + 1
+        except RateLimitedIP.DoesNotExist:
+            limited_ip = RateLimitedIP(ip_address=ip_address)
+
+        # There could be multiple users, but storing the latest should be fine for our
+        # limited intent to use it.
+        limited_ip.latest_user = user
+        limited_ip.save()
+
+
+class EdraakRateLimitModelBackend(EdraakRateLimitMixin, ModelBackend):
+    """
+    Log the locks in the database and allow fine-grained unlock.
+    """
+
+    def authenticate(self, **kwargs):
+        """
+        Authenticates a user (Django's default behaviour) and logs failed attempts in the database.
+
+        Args:
+            **kwargs: Whatever django's `ModelBackend` takes.
+        """
+        try:
+            return super(EdraakRateLimitModelBackend, self).authenticate(**kwargs)
+        except RateLimitException:
+            request = kwargs.get('request')
+            self.db_log_failed_attempt(request, kwargs[self.username_key])
+            raise  # Keep it consistent with the RateLimitMixin logic
---
+++ common/djangoapps/edraak_ratelimit/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RateLimitedIP',
+            fields=[
+                ('ip_address', models.GenericIPAddressField(serialize=False, primary_key=True)),
+                ('lockout_count', models.IntegerField(default=1, db_index=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True, db_index=True)),
+                ('latest_user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'IP-based Lock',
+                'verbose_name_plural': 'IP-based Locks',
+            },
+        ),
+    ]
