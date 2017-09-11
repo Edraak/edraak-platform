openedx/edx-platform

Add optional support for Unicode usernames
Refactoring: Use format with named variables

common/test/acceptance/pages/common/auto_auth.py
@@ -23,8 +23,8 @@ class AutoAuthPage(PageObject):
     # Internal cache for parsed user info.
     _user_info = None
 
-    def __init__(self, browser, username=None, email=None, password=None, full_name=XSS_INJECTION, staff=False, course_id=None,
-                 enrollment_mode=None, roles=None, no_login=False, is_active=True, course_access_roles=None):
+    def __init__(self, browser, username=None, email=None, password=None, full_name=XSS_INJECTION, staff=False, superuser=None,
+                 course_id=None, enrollment_mode=None, roles=None, no_login=False, is_active=True, course_access_roles=None):
         """
         Auto-auth is an end-point for HTTP GET requests.
         By default, it will create accounts with random user credentials,
@@ -33,6 +33,7 @@ class AutoAuthPage(PageObject):
         `username`, `email`, and `password` are the user's credentials (strings)
         'full_name' is the profile full name value
         `staff` is a boolean indicating whether the user is global staff.
+        `superuser` is a boolean indicating whether the user is a super user.
         `course_id` is the ID of the course to enroll the student in.
         Currently, this has the form "org/number/run"
 
@@ -49,6 +50,7 @@ class AutoAuthPage(PageObject):
         self._params = {
             'full_name': full_name,
             'staff': staff,
+            'superuser': superuser,
             'is_active': is_active,
             'course_access_roles': course_access_roles,
         }
@@ -61,6 +63,9 @@ class AutoAuthPage(PageObject):
 
         if password:
             self._params['password'] = password
+
+        if superuser is not None:
+            self._params['superuser'] = "true" if superuser else "false"
 
         if course_id:
             self._params['course_id'] = course_id

common/test/acceptance/tests/lms/test_unicode_username_admin.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+End-to-end tests for admin change view.
+"""
+
+from common.test.acceptance.pages.common.auto_auth import AutoAuthPage
+from common.test.acceptance.pages.lms.admin import ChangeUserAdminPage
+from common.test.acceptance.tests.helpers import AcceptanceTest
+
+
+class UnicodeUsernameAdminTest(AcceptanceTest):
+    """
+    Tests if it is possible to update users with unicode usernames in the admin.
+    """
+
+    # The word below reads "Omar II", in Arabic. It also contains a space and
+    # an Eastern Arabic Number another option is to use the Esperanto fake
+    # language but this was used instead to test non-western letters.
+    FIXTURE_USERNAME = u'عمر ٢'
+
+    # From the db fixture `unicode_user.json`
+    FIXTURE_USER_ID = 1000
+
+    def setUp(self):
+        """
+        Initializes and visits the change user admin page as a superuser.
+        """
+        # Some state is constructed by the parent setUp() routine
+        super(UnicodeUsernameAdminTest, self).setUp()
+
+        AutoAuthPage(self.browser, staff=True, superuser=True).visit()
+
+        # Load page objects for use by the tests
+        self.page = ChangeUserAdminPage(self.browser, self.FIXTURE_USER_ID)
+
+        # Navigate to the index page and get testing!
+        self.page.visit()
+
+    def test_update_first_name(self):
+        """
+        As a superuser I should be able to update the first name of a user with unicode username.
+        """
+        self.assertNotEqual(self.page.first_name, 'John')
+        self.assertEquals(self.page.username, self.FIXTURE_USERNAME)
+
+        self.page.change_first_name('John')
+
+        self.assertFalse(self.page.is_browser_on_page(), 'Should redirect to the admin user list view on success')
+
+        # Visit the page again to verify changes
+        self.page.visit()
+
+        self.assertEquals(self.page.first_name, 'John', 'The first name should be updated')

Edraak/edraak-platform

common/test/acceptance/pages/lms/auto_auth.py
@@ -17,8 +17,8 @@ class AutoAuthPage(PageObject):
 
     CONTENT_REGEX = r'.+? user (?P<username>\S+) \((?P<email>.+?)\) with password \S+ and user_id (?P<user_id>\d+)$'
 
-    def __init__(self, browser, username=None, email=None, password=None, full_name=None, staff=None, course_id=None,
-                 enrollment_mode=None, roles=None):
+    def __init__(self, browser, username=None, email=None, password=None, full_name=None, staff=None, superuser=None,
+                 course_id=None, enrollment_mode=None, roles=None):
         """
         Auto-auth is an end-point for HTTP GET requests.
         By default, it will create accounts with random user credentials,
@@ -27,6 +27,7 @@ class AutoAuthPage(PageObject):
         `username`, `email`, and `password` are the user's credentials (strings)
         'full_name' is the profile full name value
         `staff` is a boolean indicating whether the user is global staff.
+        `superuser` is a boolean indicating whether the user is a super user.
         `course_id` is the ID of the course to enroll the student in.
         Currently, this has the form "org/number/run"
 
@@ -53,6 +54,9 @@ class AutoAuthPage(PageObject):
 
         if staff is not None:
             self._params['staff'] = "true" if staff else "false"
+
+        if superuser is not None:
+            self._params['superuser'] = "true" if superuser else "false"
 
         if course_id is not None:
             self._params['course_id'] = course_id

common/test/acceptance/tests/lms/test_unicode_username_admin.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+End-to-end tests for admin change view.
+"""
+
+from common.test.acceptance.pages.lms.auto_auth import AutoAuthPage
+from common.test.acceptance.pages.lms.admin import ChangeUserAdminPage
+from common.test.acceptance.tests.helpers import AcceptanceTest
+
+
+class UnicodeUsernameAdminTest(AcceptanceTest):
+    """
+    Tests if it is possible to update users with unicode usernames in the admin.
+    """
+
+    # The word below reads "Omar II", in Arabic. It also contains a space and
+    # an Eastern Arabic Number another option is to use the Esperanto fake
+    # language but this was used instead to test non-western letters.
+    FIXTURE_USERNAME = u'عمر ٢'
+
+    # From the db fixture `unicode_user.json`
+    FIXTURE_USER_ID = 1000
+
+    def setUp(self):
+        """
+        Initializes and visits the change user admin page as a superuser.
+        """
+        # Some state is constructed by the parent setUp() routine
+        super(UnicodeUsernameAdminTest, self).setUp()
+
+        AutoAuthPage(self.browser, staff=True, superuser=True).visit()
+
+        # Load page objects for use by the tests
+        self.page = ChangeUserAdminPage(self.browser, self.FIXTURE_USER_ID)
+
+        # Navigate to the index page and get testing!
+        self.page.visit()
+
+    def test_update_first_name(self):
+        """
+        As a superuser I should be able to update the first name of a user with unicode username.
+        """
+        self.assertNotEqual(self.page.first_name, 'John')
+        self.assertEquals(self.page.username, self.FIXTURE_USERNAME)
+
+        self.page.change_first_name('John')
+
+        self.assertFalse(self.page.is_browser_on_page(), 'Should redirect to the admin user list view on success')
+
+        # Visit the page again to verify changes
+        self.page.visit()
+
+        self.assertEquals(self.page.first_name, 'John', 'The first name should be updated')