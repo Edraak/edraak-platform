9ebb311a8b5a68dce16eef54ccb47853b999c848
Shadi Naif <shadinaif@gmail.com>
2019-04-21 15:43:50 +0300

    Missing pip packs for Edraak Certificate

--- scripts/jenkins-common.sh
+++ scripts/jenkins-common.sh
@@ -75,4 +75,5 @@ SHARD_NUM=${SHARD:="all"}
 export PAVER_TIMER_LOG="test_root/log/timing.paver.$TEST_SUITE.$SHARD_NUM.log"
 export ANSIBLE_TIMER_LOG="test_root/log/timing.ansible.$TIMESTAMP.log"
 
-echo "This node is `curl http://169.254.169.254/latest/meta-data/hostname`"
+# The following line is only used in AWS
+# echo "This node is `curl http://169.254.169.254/latest/meta-data/hostname`"
