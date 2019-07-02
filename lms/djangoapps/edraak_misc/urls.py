e9f7bc3560c2f140acdff699666391e7f099042c
Shadi Naif <shadinaif@gmail.com>
2019-07-02 14:03:57 +0300

    Remove unused code from edraak_misc

--- lms/djangoapps/edraak_misc/urls.py
+++ lms/djangoapps/edraak_misc/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 
-from edraak_misc.views import all_courses, check_student_grades, student_course_complete_status
+from edraak_misc.views import check_student_grades, student_course_complete_status
 
 
 urlpatterns = [
@@ -9,11 +9,6 @@ urlpatterns = [
         r'^check_student_grades/$',
         check_student_grades,
         name='edraak_check_student_grades'
-    ),
-    url(
-        r'^all_courses/$',
-        all_courses,
-        name='edraak_all_courses'
     ),
     url(
         r'^course_complete_status/{}$'.format(settings.COURSE_ID_PATTERN),
