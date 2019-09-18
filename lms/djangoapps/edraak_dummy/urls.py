7a21a8313676c833f6115c7d6c74d187af136a92
Ali H <devalih@gmail.com>
2019-09-18 15:46:42 +0300

    marketing trying to fix csrf issue, HACK

--- lms/djangoapps/edraak_dummy/urls.py
+++ lms/djangoapps/edraak_dummy/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from edraak_dummy.views import SetCSRFDummyView
 
-urlpatterns = patterns('',  # nopep8
-    url('^edraak_set/csrf', SetCSRFDummyView.as_view(), name='edraak_setcsrf'),
-)
+urlpatterns = [
+    url('^edraak_set/csrf', SetCSRFDummyView.as_view(), name='edraak_setcsrf')
+]
