33c9ad62303ac2b296f8abdf022fad1ee1c4aae9
Ali H <devalih@gmail.com>
2019-09-26 18:16:26 +0300

    get CSRF_COOKIE_DOMAIN back !

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -199,6 +199,7 @@ EDXMKTG_USER_INFO_COOKIE_NAME = ENV_TOKENS.get('EDXMKTG_USER_INFO_COOKIE_NAME', 
 # but it is highly recommended that this is True for environments accessed
 # by end users.
 CSRF_COOKIE_SECURE = ENV_TOKENS.get('CSRF_COOKIE_SECURE', False)
+CSRF_COOKIE_DOMAIN = ENV_TOKENS.get('CSRF_COOKIE_DOMAIN', "")
 
 #Email overrides
 DEFAULT_FROM_EMAIL = ENV_TOKENS.get('DEFAULT_FROM_EMAIL', DEFAULT_FROM_EMAIL)
--- cms/envs/common.py
+++ cms/envs/common.py
@@ -456,6 +456,7 @@ CSRF_COOKIE_AGE = 60 * 60 * 24 * 7 * 52
 # It is highly recommended that you override this in any environment accessed by
 # end users
 CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_DOMAIN = ""
 
 #################### CAPA External Code Evaluation #############################
 XQUEUE_INTERFACE = {
