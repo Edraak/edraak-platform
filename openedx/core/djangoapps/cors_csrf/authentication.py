e642cdb276e167e9048e566e78119d73e5eecaf8
Ali <ahasan@qrf.org>
2020-04-01 23:53:06 +0300

    More of Use SessionAuthenticationAllowInactiveUser to fix a ton of 403 errors

--- openedx/core/djangoapps/cors_csrf/authentication.py
+++ openedx/core/djangoapps/cors_csrf/authentication.py
@@ -9,6 +9,8 @@ from .helpers import is_cross_domain_request_allowed, skip_cross_domain_referer_
 
 
 class SessionAuthenticationCrossDomainCsrf(SessionAuthenticationAllowInactiveUser):
+    # TODO: Edraak-specific:
+    #   At Edraak we want to allow inactive users full access
     """
     Session authentication that skips the referer check over secure connections.
 
