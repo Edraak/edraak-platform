e8a50b404761c43915627f7e79f26bce54e5b6bf
Shadi Naif <shadinaif@gmail.com>
2020-08-19 12:48:47 +0300

    Add new API edraak_course_list

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -5,7 +5,13 @@ URLs for the Enrollment API
 from django.conf import settings
 from django.conf.urls import url
 
-from .views import EnrollmentCourseDetailView, EnrollmentListView, EnrollmentView, UnenrollmentView
+from .views import (
+    EdraakCourseListView,
+    EnrollmentCourseDetailView,
+    EnrollmentListView,
+    EnrollmentView,
+    UnenrollmentView,
+)
 
 urlpatterns = [
     url(r'^enrollment/{username},{course_key}$'.format(
@@ -18,4 +24,10 @@ urlpatterns = [
     url(r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(), name='courseenrollmentdetails'),
     url(r'^unenroll/$', UnenrollmentView.as_view(), name='unenrollment'),
+
+    url(
+        r'^edraak_course_list$',
+        EdraakCourseListView.as_view(),
+        name='edraak_course_list'
+    ),
 ]
