cd470c88ef57ab3b26f9feb9ebaa4c2bb987ffee
Ali <ahasan@qrf.org>
2021-02-22 17:22:25 +0200

    Allow users with unstable password to change thier passwords

--- common/djangoapps/student/forms.py
+++ common/djangoapps/student/forms.py
@@ -67,9 +67,9 @@ class PasswordResetFormNoActive(PasswordResetForm):
         
         if not len(self.users_cache):
             raise forms.ValidationError(self.error_messages['unknown'])
-        if (retirement_status is None) and any((user.password.startswith(UNUSABLE_PASSWORD_PREFIX))
-               for user in self.users_cache):
-            raise forms.ValidationError(self.error_messages['unusable'])
+        # if (retirement_status is None) and any((user.password.startswith(UNUSABLE_PASSWORD_PREFIX))
+        #        for user in self.users_cache):
+        #     raise forms.ValidationError(self.error_messages['unusable'])
 
         # if not len(self.users_cache):
         #     raise forms.ValidationError(self.error_messages['unknown'])
