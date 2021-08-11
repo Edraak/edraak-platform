d2efefe6b205c1404e779f5a363f67f8de1a0e52
OmarEdraak <47500477+OmarEdraak@users.noreply.github.com>
2021-08-11 07:22:49 +0300

    fixup

--- common/djangoapps/enrollment/data.py
+++ common/djangoapps/enrollment/data.py
@@ -75,7 +75,7 @@ def get_course_enrollments(user_id):
             log.warning(
                 (
                     u"Course enrollments for user %s reference "
-                    u"courses that do not exist (this can occur if a course is deleted). %s"
+                    u"courses that do not exist (this can occur if a course is deleted)."
                 ), user_id,
             )
 
